2e12140f35df6928dc8546d006a94ab8277b5f69
tristanmyers <tristanmyers.developer@gmail.com>
2021-08-03 11:58:37 -0400

    added player drawing, win condition, display image

--- scripts/index.js
+++ scripts/index.js
@@ -1,90 +1,68 @@
-// ISSUE: playerhand has no way of keeping track the of the suit
-class Deck {
-    constructor() {
-        this.cards = {
-            "clubs": ['King', 'Queen', 'Jack', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "spades": ['King', 'Queen', 'Jack', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "hearts": ['King', 'Queen', 'Jack', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "diamonds": ['King', 'Queen', 'Jack', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        };
-        // this.shuffledCards = [Math.Floor(Math.random(this.card))]
-        this.cardsAvailable = this.cards;
-        this.playerHand = [];
-        
-        this.playerScore = 0;
-    }
+let deckId = '';
+let playerScore = 0;
+let playerCurrentHand = [];
 
-    shuffleDeck() {
-        // this will iterate through the available cards  and return the shuffledDeck in the constructor
-        // we wanted to map to create a shuffled array of objects
-        Object.keys(this.cardsAvailable).map((key, index) => {
-            let something = this.cardsAvailable[key];
-            console.log(something);
-        })    
-    }
-
-    score() {
-        if (this.playerHand == "King" || this.playerHand == "Jack" || this.playerHand == "Queen") {
-            this.playerScore += 10;
-            return this.playerScore;
-        }
-    }
-
-    // player clicks hit, passCard() gets called
-    // 1 card gets passed from the shuffledDeck to the playerHand
-    // we this.cardsAvailable[Math.floor(Math.random()),Math.floor(Math.random())].shift()
-    // ISSUE: newPlayerHand becomes a multi dimensional array
-    passCard() {
-        // let newPlayerHand = [];
-        // for (let i = 0; i < cardsToDeal; i++) {
-        //     let shiftedCard = this.cardsAvailable["spades"].shift();
-        //     newPlayerHand.push(shiftedCard);
-        // }
-
-        // return this.playerHand.push(newPlayerHand);
-    }
-}
-
-// start game
-// shuffleDeck(), call the passCard(), which will check if player has cards then will deal 2 if the player doesn't
-// update score constantly throughout the game
 function startGame() {
-    let newDeck = new Deck();
-    newDeck.passCard();
-    newDeck.score();
-    console.log(newDeck);
-    console.log(`available cards: ${newDeck.cardsAvailable["spades"]}`);
+	// getting deck_id from api
+	fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+		.then(res => res.json()) // parse response as JSON
+		.then(data => {
+			deckId = data.deck_id
+
+		})
+		.catch(err => {
+			console.log(`error ${err}`)
+		});
+
+	// player decks
+	let player1Draw = document.querySelector('#player1-draw-card');
+	let player2Draw = document.querySelector('#player2-draw-card');
+
+	player1Draw.addEventListener('click', () => {
+		drawCards(1, "player1");
+	});
+
+	player2Draw.addEventListener('click', () => {
+		drawCards(1, "player2");
+	});
+
+	// drawing a card from the deck, displaying the image of the card
+	function drawCards(amountOfCards, playerPressed) {
+
+		const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${amountOfCards}`
+
+		let player1Hand;
+		let player2Hand;
+
+		fetch(url)
+			.then(res => res.json()) // parse response as JSON
+			.then(data => {
+				console.log(data.cards[0])
+
+				// display seperate card based on which player pressed it
+				if (playerPressed == "player1") {
+					document.querySelector('#card1').src = data.cards[0].image;
+					player1Hand = data.cards[0].value;
+					console.log(player1Hand);
+				} else if (playerPressed == "player2") {
+					document.querySelector('#card2').src = data.cards[0].image;
+					player2Hand = data.cards[0].value;
+					console.log(player2Hand);
+				}
+
+				// win condition
+				if (player1Hand == "JACK") {
+					document.querySelector("#win-container").innerHTML = "PLAYER 1 WINS";
+				} else if(player2Hand == "JACK") {
+					document.querySelector("#win-container").innerHTML = "PLAYER 2 WINS";
+				}
+
+			})
+			.catch(err => {
+				console.log(`error ${err}`)
+			});
+
+	}
 }
 
 startGame()
-
-
-// generating an array with random numbers
-// this could be making the time complexity worse 
-var makeRandomArray = function (min, max, amountOfNumbers) {
-    var randomArray = [];
-    // getting a random number, converting from float to whole number, pushing to array, and looping as many times as amountOfNumbers param
-    for (var i = 0; i < amountOfNumbers; i++) {
-        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        randomArray.push(randomNumber);
-    }
-    console.log(randomArray);
-    return randomArray;
-};
-// var generatedRandomArray = makeRandomArray(1, 1000, 100);
-
-
-
-
-
-// const persons = [
-//     { firstname: "Malcom", lastname: "Reynolds" },
-//     { firstname: "Kaylee", lastname: "Frye" },
-//     { firstname: "Jayne", lastname: "Cobb" }
-// ];
-
-// persons.map(getFullName);
-
-// function getFullName(item) {
-//     return = [item.firstname, item.lastname].join(" ");
-// }
